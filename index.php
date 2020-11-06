3461cdb57f1715105bf245bebd5d38f127053e99
chattarin63070025 <73661033+chattarin63070025@users.noreply.github.com>
2020-11-06 12:28:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,7 @@
 </head>
 
 <body>
-    <h1>My frist heading</h1>
-    <p>My first paragraph</p>
-    <img src="cat1.jpg">
-    <img src="cat2.jpg">
-    <br>
-    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
-
-    <h2>funny</h2>
-    <p>save<br>is<br>very smart<br>in<br>it</in>
-    </p>
+    <a href = "Card.html"> Work 1 </a>
 
 
 </body>
